a9733debdf82357a60c2bffa2775d3a37114e2ee
denis909 <denis909@mail.ru>
2020-07-20 17:35:27 +0300

    n/a

---
+++ LogModelTarget.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace denis909\yii;
+
+use Yii;
+use Exception;
+use Throwable;
+use denis909\yii\ModelException;
+
+class LogModelTarget extends \yii\log\Target
+{
+
+    public $modelClass;
+
+    public function export()
+    {
+        Assert::notEmpty($this->modelClass, get_called_class() . '::modelClass is empty.');
+
+        foreach ($this->messages as $message)
+        {
+            list($text, $level, $category, $timestamp) = $message;
+            
+            if (!is_string($text))
+            {
+                if ($text instanceof Throwable || $text instanceof Exception)
+                {
+                    $text = (string) $text;
+                }
+            }
+
+            $model = Yii::createObject([
+                'level' => $level,
+                'category' => $category,
+                'log_time' => $timestamp,
+                'prefix' => $this->getMessagePrefix($message),
+                'message' => $text
+            ]);
+
+            if (!$model->save())
+            {
+                throw new ModelException($model);
+            }
+        }
+    }
+
+}
